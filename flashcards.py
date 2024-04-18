a7d0f13c8cd57db11045328ddc742e8c5edcb7b9
Jacek Spalinski <jfspalinski@gmail.com>
2024-04-18 14:52:12 +0200

    Add verification for double terms/definitions

--- flashcards.py
+++ flashcards.py
@@ -1,13 +1,23 @@
 num_of_cards = int(input("Input the number of cards:\n"))
-cards = []
+cards = {}
 for i in range(num_of_cards):
     term = input(f"The term for card #{i + 1}:\n")
-    definition = input(f"The definition for card #{i + 1}:\n")
-    cards.append((term, definition))
+    while term in cards.keys():
+        term = input(f'The term "{term}" already exists. Try again:\n')
 
-for term, definition in cards:
+    definition = input(f"The definition for card #{i + 1}:\n")
+    while definition in cards.values():
+        definition = input(f'The definition "{definition}" already exists. Try again:\n')
+
+    cards[term] = definition
+
+for term, definition in cards.items():
     answer = input(f'Print the definition of "{term}":\n')
     if answer == definition:
         print("Correct!")
+    elif answer in cards.values():
+        correct_term = [aux_term for aux_term in cards if cards[aux_term] == answer]
+        print(f'Wrong. The right answer is "{definition}", \
+                but your definition is correct for "{correct_term}".')
     else:
         print(f'Wrong. The right answer is "{definition}".')
