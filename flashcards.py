43c444dfc84adcf599f9152a00c748df4bf48421
Jacek Spalinski <jfspalinski@gmail.com>
2024-04-20 18:31:05 +0200

    Add mistake counter and option to save stdout to log file

--- flashcards.py
+++ flashcards.py
@@ -1,10 +1,14 @@
-from utils import add_card, remove_card, import_cards, export_cards, ask_about_cards
+from utils import (get_and_log_user_input, add_card, remove_card, import_cards,
+                   export_cards, ask_about_cards, write_logs,
+                   find_the_hardest_terms, reset_mistakes_count)
 
 
 def start_menu():
+    # sys.stdout = logger = Logger()  # redirect stdout to Logger class
     cards = {}
     while True:
-        command = input("\nInput the action (add, remove, import, export, ask, exit):\n")
+        command = get_and_log_user_input("\nInput the action (add, remove, import, "
+                                         "export, ask, exit, log, hardest card, reset stats):")
         match command:
             case "add":
                 cards = add_card(cards)
@@ -15,7 +19,13 @@ def start_menu():
             case "export":
                 export_cards(cards)
             case "ask":
-                ask_about_cards(cards)
+                cards = ask_about_cards(cards)
+            case "log":
+                write_logs()
+            case "hardest card":
+                find_the_hardest_terms(cards)
+            case "reset stats":
+                reset_mistakes_count(cards)
             case "exit":
                 print("Bye bye!")
                 break
